8ccde94c610b14706e56e8ffec049c4862aaee2d
Rudolf Hartmann <rudolf.hartmann@trueinnovation.de>
2018-08-15 16:52:02 +0200

    extend user.model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -2,9 +2,23 @@ import mongoose from 'mongoose';
 
 const UserSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    email: { type: String, required: true },
-    password: { type: String, required: true }
+    email: {
+        type: String,
+        unique: true,
+        required: [true, 'E-Mail is required']
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    },
+    updated: {
+        type: Date,
+        default: Date.now
+    }
 });
-
 
 export default mongoose.model('User', UserSchema);
